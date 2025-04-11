3d2e5fd12175a46a1998af57665e45bbeade95fa
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2025-04-11 10:55:11 -0700

    added course_reqs.py

---
+++ course_reqs.py
@@ -0,0 +1,76 @@
+UC_REQUIREMENTS = {
+    "UCSD": {
+        "Java Programming": [["CSE 8B", "B", 1], ["CSE 11", "A", 1]],  # One of
+        "Data Strucutres": ["CSE 12", "A", 1],
+        "C": {"CSE 20"},
+        "D": {"MATH 20A"},
+        "E": {"MATH 20B"},
+        "F": {"MATH 20C"},
+        "G": {"MATH 18"},
+    },
+    "UCI": {
+        "A": {"MATH 2A"},
+        "B": {"MATH 2B"},
+        "C": {"I&C SCI 31"},
+        "D": {"I&C SCI 32"},
+        "E": {"I&C SCI 33"}
+        # "F": {"I&C SCI 46", "IN4MATX 43", "I&C SCI 6B", "I&C SCI 6D", "MATH 3A", "I&C SCI 6N"}
+    },
+    "UCD": {
+        "A": {"MAT 021A"},
+        "B": {"MAT 021B"},
+        "C": {"MAT 021C"},
+        "D": {"ECS 020"},
+        "E": {"ECS 036A"},
+        "F": {"ECS 036B"},
+        "G": {"ECS 036C"},
+        "H": {"ECS 050"}
+    },
+    "UCR": {
+        "A": {"CS 10A"},
+        "B": {"CS 10B"},
+        "C": {"MATH 9A"},
+        "D": {"MATH 9B"},
+        "E": {"MATH 9C"}
+        # "G": {"CS 11", "CS 10C", "CS 61", "MATH 10A", "PHYS 40B", "PHYS 40C"}  # choose 3
+    },
+    "UCLA": {
+        "A": {"MATH 31A"},  # Assuming generic naming
+        "B": {"MATH 31B"},
+        "C": {"MATH 32A"},
+        "D": {"MATH 32B"},
+        "E": {"MATH 33A"},
+        "G": {"COM SCI 31"}
+    },
+    "UCB": {
+        "A": {"MATH 1A"},
+        "B": {"MATH 1B"},
+        "C": {"MATH 53"},
+        "D": {"MATH 54"},
+    },
+    "UCM": {
+        "C": {"CSE 020"},
+        "D": {"CSE 021"},
+        "E": {"CSE 030"},
+        "F": {"MATH 021"},
+        "G": {"MATH 022"},
+        "H": {"MATH 023"},
+        "I": {"MATH 024"},
+    },
+    "UCSC": {
+        "A": {"CSE 12"},
+        "B": {"CSE 16"},
+        "C": {"CSE 30"},
+        "D": {"MATH 19A", "MATH 20A"},  # one of
+        "E": {"MATH 19B", "MATH 20B"}   # one of
+    },
+    "UCSB": {
+        "A": {"MATH 3A"},
+        "B": {"MATH 3B"},
+        "C": {"MATH 4A"},
+        "D": {"MATH 4B"},
+        "E": {"CMPSC 16"},
+        "F": {"CMPSC 24"},
+        "G": {"CMPSC 40"}
+    }
+}
